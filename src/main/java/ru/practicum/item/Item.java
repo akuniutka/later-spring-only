00c136112407909ea9c8de5be62ceab9c7629a2e
Andrei Kuniutka <akuniutka@gmail.com>
2024-09-13 21:57:01 +0300

    Fix: Make User abd Item JPA Entities

--- src/main/java/ru/practicum/item/Item.java
+++ src/main/java/ru/practicum/item/Item.java
@@ -1,11 +1,23 @@
 package ru.practicum.item;
 
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
 public class Item {
 
+    @Id
     private Long id;
+
+    @Column(name = "user_id", nullable = false)
     private Long userId;
+
     private String url;
 }
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -1,11 +1,36 @@
 package ru.practicum.user;
 
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.time.Instant;
+
+@Entity
+@Table(name = "users", schema = "public")
+@Getter
+@Setter
 public class User {
 
+    @Id
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     private String email;
-    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+
+    @Column(name = "registration_date")
+    private Instant registrationDate = Instant.now();
 }
